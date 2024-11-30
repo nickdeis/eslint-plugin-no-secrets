2f6ee21de16fa429024ecb43fa45ec9657b71c17
Nicholas Deis <nicholasdeis@Nicholass-MacBook-Air.local>
2024-11-29 19:55:40 -0700

    Remove type files

--- dist/no-pattern-match.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Rule } from "eslint";
-declare const noPatternMatch: Rule.RuleModule;
-export default noPatternMatch;
--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
-import type { ESLint, Rule } from 'eslint';
+import type { ESLint, Rule } from "eslint";
 
 declare const eslintPluginNoSecrets: ESLint.Plugin & {
-    rules: {
-        'no-secrets': Rule.RuleModule,
-    };
+  rules: {
+    "no-secrets": Rule.RuleModule;
+    "no-pattern-match": Rule.RuleModule;
+  };
 };
 
 export = eslintPluginNoSecrets;
